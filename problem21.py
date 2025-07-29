eb0549107664a0280b13d6a9c7ced05cde54183e
LovejeetM <lovejeetmatharu54@gmail.com>
2025-07-29 12:33:56 +0530

    22

--- problem21.py
+++ problem21.py
@@ -57,3 +57,24 @@ def maxProfit(prices):
         
 prices = [7,1,5,3,6,4]
 print(maxProfit(prices))
+
+
+
+"""
+Best sol: 
+
+def maxProfit(prices):
+        
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                profit = price - min_price
+                if profit > max_profit:
+                    max_profit = profit
+                    
+        return max_profit
+"""
