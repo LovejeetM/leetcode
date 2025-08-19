f37905f64e3ff27dcf803a5411568c10e14e20e0
LovejeetM <lovejeetmatharu54@gmail.com>
2025-08-19 15:40:59 +0530

    33

--- leetcode/problem33.py
+++ leetcode/problem33.py
@@ -34,10 +34,50 @@ What if nums1's size is small compared to nums2's size? Which algorithm is bette
 What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 """
 
-def intersect(self, nums1, nums2):
+def intersect(nums1, nums2):
     """
     :type nums1: List[int]
     :type nums2: List[int]
     :rtype: List[int]
     """
+    
+    inter = []
+    nums1.sort()
+    nums2.sort()
+    
+
+    used = set()
+    used1 = set()
+    for i, ch in enumerate(nums1):
+        for j, char in enumerate(nums2):
+            if j not in used1 and i not in used and ch == char:
+                used1.add(j)
+                used.add(i)
+
+    # print(used)
+    inter = [nums1[i] for i in used]
+    return inter
+
+    # inter = []
+    # nums1.sort()
+    # nums2.sort()
+    # used = {}
+    
+    # for ch in nums1:
+    #     for char in nums2:
+    #         if ch not in used and ch == char:
+    #             used[ch] = 1
+    #         if ch in used and ch == char:
+    #             used[ch] += 1
+    
+
+    # print(used.items())
+    # for c in used:
+    #     for i in range(0, used[c]-1):
+    #         inter.append(c)
+    # return inter
+
+nums1 = [1,2,2,1]
+nums2 = [2]
+print(intersect(nums1, nums2))
     
