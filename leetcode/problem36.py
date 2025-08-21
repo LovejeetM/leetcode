c622033d510502909ad4593ebc776a8d51e6919c
LovejeetM <lovejeetmatharu54@gmail.com>
2025-08-21 12:29:59 +0530

    36

--- leetcode/problem36.py
+++ leetcode/problem36.py
@@ -31,10 +31,20 @@ Explanation: 1248 is divisible by all of its digits, hence the answer is 4.
 """
 
 
-def countDigits(self, num):
+def countDigits(num):
     """
     :type num: int
     :rtype: int
     """
+    s = str(num)
+    ct = 0
+    for i in s:
+        if num % int(s) == 0:
+            ct += 1
+    
+    return ct
 
+
+num = 1248
+print(countDigits(num))
     
