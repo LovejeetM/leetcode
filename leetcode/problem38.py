7e7810650cb5e439c559b3dd2f699db2f85bc3ab
LovejeetM <lovejeetmatharu54@gmail.com>
2025-08-22 10:47:55 +0530

    38

--- leetcode/problem38.py
+++ leetcode/problem38.py
@@ -46,4 +46,51 @@ grid[i][j] is either 0 or 1.
 The input is generated such that there is at least one 1 in grid.
 """
 
+def minimumArea(grid):
+    """
+    :type grid: List[List[int]]
+    :rtype: int
+    """
+    dim = []
+    for i, row in enumerate(grid):
+        if 1 in row:
+            dim.append(i)
+            break
 
+    if not dim:
+        return 0
+
+
+    for j in range(len(grid[0])):
+        for i in range(len(grid)):
+            if grid[i][j] == 1:
+                dim.append(j)
+                break
+        if len(dim) == 2:
+            break
+
+    for i in range(len(grid) - 1, -1, -1):
+        if 1 in grid[i]:
+            dim.append(i)
+            break
+        if len(dim) == 3:
+            break
+
+    for j in range(len(grid[len(grid)-1])-1, -1, -1):
+        # for i in range(len(grid) -1, -1, -1):
+        for i in range(len(grid)):
+            if grid[i][j] == 1:
+                dim.append(j)
+                break
+        if len(dim) == 4:
+            break
+
+    
+    height = dim[2] - dim[0] + 1
+    width = dim[3] - dim[1] + 1
+    
+    return height * width
+
+            
+grid = [[0,0,0,0,0],[0,0,1,0,0],[0,0,0,0,0],[0,1,0,1,0],[0,0,0,0,0]]
+print(minimumArea(grid))
